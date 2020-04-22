0c2cb3a92851251ecce6f0d8ed8e219519103fdc
Jeff Held <jeff.held@mcmakler.de>
2020-04-22 11:45:37 +0200

    preload language before app bootstraps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { TranslocoRootModule } from './transloco-root.module';
 import { LanguageSelectComponent } from './language-select/language-select.component';
+import { LanguageService } from './language.service';
+
+function preloadLanguage(languageService: LanguageService) {
+  return () => languageService.preloadLanguage();
+}
 
 @NgModule({
   declarations: [AppComponent, LanguageSelectComponent],
   imports: [BrowserModule, HttpClientModule, TranslocoRootModule],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: preloadLanguage,
+      multi: true,
+      deps: [LanguageService],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
